b283be0333f14ee8173b70f5a135504f310ce875
djiq <magicznyszop@gmail.com>
2024-01-08 20:50:42 +0100

    e

--- src/weapons.cpp
+++ src/weapons.cpp
@@ -29,12 +29,15 @@ void WeaponSimpleGun::Tick(double playerx, double playery){
 void WeaponSimpleGun::OnLeftMBPressed(double playerx, double playery){
     Vector2 pos = GetMousePositionInWorld();
     ECS* ecs = ECS::instance();
-    float x = pos.x - playerx;
-    float y = pos.y - playery;
-    float angle = atan2f(y,x);
-    EntityBuilder().AddComponent(new DrawComponent("assets/textures/bullet.png"))
-                    .AddComponent(new PositionComponent(playerx+16,playery+16,4,4))
-                    .AddComponent(new BulletComponent(angle,bullet_damage,10,bullet_range))
-                    .Build();
+    for(int i = 0; i < bullet_amt; i ++){
+        float x = pos.x - playerx;
+        float y = pos.y - playery;
+        float angle = atan2f(y,x);
+        angle += (rand() % (2 * bullet_spread_arc)) - bullet_spread_arc
+        EntityBuilder().AddComponent(new DrawComponent("assets/textures/bullet.png"))
+                        .AddComponent(new PositionComponent(playerx+16,playery+16,4,4))
+                        .AddComponent(new BulletComponent(angle,bullet_damage,10,bullet_range))
+                        .Build();
+    }
 
 };
